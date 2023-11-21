137f77bb6e7bfa85c88b394b85218dcab8222b6b
MD. NIZAM UDDIN TUHIN <112401451+md-nizam-uddin-tuhin-bhl@users.noreply.github.com>
2023-11-21 09:43:47 +0600

    Update IStudent.cs

--- StudentAdminPotal/Repository/IStudent.cs
+++ StudentAdminPotal/Repository/IStudent.cs
@@ -5,7 +5,7 @@ namespace StudentAdminPotal.Repository
     public interface IStudent
     {
         Task<List<Student>> GetStudentsAsync();
-<<<<<<< HEAD
+
 
         Task<Student> GetStudentAsync(Guid studentId);
 
@@ -17,13 +17,10 @@ namespace StudentAdminPotal.Repository
         Task<Student> DeleteStudent(Guid studentId);
         Task<Student> AddStudent(Student request);
 
-=======
-<<<<<<< HEAD
+
         Task<Student> GetStudentAsync(Guid studentId);
 
         Task<List<Gender>> GetGendersAsync();
-=======
->>>>>>> e934563d2c3c35585eb74d084d55a82c7c8d6c6f
->>>>>>> 0287b490315fa22b224992bdc4d7af4056f0f83f
+
     }
 }
